d56361fb6c002cab1e7df7febc97847d883d1bd8
Ricky Ding <416640656@qq.com>
2026-05-05 03:40:53 +0800

    update test: add RoPE, RMSNorm, SwiGLU

--- python/tests/test.py
+++ python/tests/test.py
@@ -2,14 +2,39 @@ import jax.numpy as jnp
 import optax
 import tiktoken
 from flax import nnx
-from jax import Array, random
-from jax.nn import gelu, softmax
+from jax import Array, lax, random
+from jax.nn import silu, softmax
 from jax.random import PRNGKey, categorical
+
+
+class RoPE:
+    def __init__(s, T, d_head, base=1e4):
+        omega = 1.0 / (base ** (jnp.arange(0, d_head, 2) / d_head))
+        s.e_iwt = jnp.exp(1j * jnp.outer(jnp.arange(T), omega))
+
+    def __call__(s, q: Array):
+        B, n_head, T, d_head = q.shape
+        z = lax.complex(q[..., ::2], q[..., 1::2]) * s.e_iwt[:T]
+        return jnp.stack([jnp.real(z), jnp.imag(z)], axis=-1).reshape(q.shape)
+
+
+class RMSNorm(nnx.Module):
+    def __init__(s, dim, eps=1e-6):
+        s.gain = nnx.Param(jnp.ones(dim))
+        s.eps = eps
+
+    def __call__(s, x):
+        MS = jnp.mean(jnp.square(x), axis=-1, keepdims=True)
+        return x * lax.rsqrt(MS + s.eps) * s.gain
+
+
+# =========================================
 
 
 class LLMConf:
     n_vocab: int
     mask: Array
+    roPE: RoPE
 
     max_T: int = 1000
     n_head: int = 8
@@ -18,6 +43,13 @@ class LLMConf:
     d_ff: int = 64
     dropout = 0.1
     rngs = nnx.Rngs(0)
+
+    @property
+    def d_head(s):
+        return s.d_model // s.n_head
+
+    def linear(s, a, b):
+        return nnx.Linear(a, b, use_bias=False, rngs=s.rngs)
 
 
 def to_batch(seq, T):
@@ -39,15 +71,10 @@ class TextEmbed(nnx.Module):
     def __init__(s, c: LLMConf):
         s.c = c
         s.token_embed = nnx.Embed(c.n_vocab, c.d_model, rngs=c.rngs)
-        s.pos_embed = nnx.Embed(c.max_T, c.d_model, rngs=c.rngs)
 
     def __call__(s, x: Array):
         # x: (B, T)
-        T = x.shape[1]
-        token_vec = s.token_embed(x) * jnp.sqrt(s.c.d_model)
-        pos_idx = jnp.arange(T)[None, :]
-        pos_vec = s.pos_embed(pos_idx)
-        return token_vec + pos_vec
+        return s.token_embed(x) * jnp.sqrt(s.c.d_model)
 
     def undo(s, x: Array):
         W: Array = s.token_embed.embedding
@@ -59,13 +86,13 @@ class MultiHeadAttention(nnx.Module):
         s.c = c
         assert c.d_model % c.n_head == 0
         s.d_head = c.d_model // c.n_head
-        s.WQ = nnx.Linear(c.d_model, c.d_model, rngs=c.rngs)
-        s.WK = nnx.Linear(c.d_model, c.d_model, rngs=c.rngs)
-        s.WV = nnx.Linear(c.d_model, c.d_model, rngs=c.rngs)
-        s.WO = nnx.Linear(c.d_model, c.d_model, rngs=c.rngs)
+        s.WQ = c.linear(c.d_model, c.d_model)
+        s.WK = c.linear(c.d_model, c.d_model)
+        s.WV = c.linear(c.d_model, c.d_model)
+        s.WO = c.linear(c.d_model, c.d_model)
 
     def split_heads(s, x: Array):
-        B, T, _ = x.shape
+        B, T, d_model = x.shape
         x = x.reshape(B, T, s.c.n_head, s.d_head)
         return x.transpose(0, 2, 1, 3)  # (B, n_head, T, d_head)
 
@@ -74,31 +101,39 @@ class MultiHeadAttention(nnx.Module):
         return x.transpose(0, 2, 1, 3).reshape(B, T, n_head * d_head)
 
     def __call__(s, x: Array):
-        T = x.shape[1]
+        B, T, d_model = x.shape
         q, k, v = s.WQ(x), s.WK(x), s.WV(x)
-        q, k, v = map(s.split_heads, [q, k, v])
+        q, k, v = map(s.split_heads, (q, k, v))
         # (B, n_head, T, d_head) @ (B, n_head, d_head, T) -> (B, n_head, T, T)
-        qk = jnp.matmul(q, k.transpose(0, 1, 3, 2))
-        qk = qk / jnp.sqrt(s.d_head)
+        q, k = map(s.c.roPE, (q, k))
+        qk = jnp.matmul(q, k.transpose(0, 1, 3, 2)) / jnp.sqrt(s.d_head)
         if s.c.mask is not None:
             qk = jnp.where(s.c.mask[:T, :T], -jnp.inf, qk)
         context = jnp.matmul(softmax(qk, axis=-1), v)
         return s.WO(s.unsplit_heads(context))
 
 
+class SwiGLU(nnx.Module):
+    def __init__(s, c: LLMConf):
+        s.W1 = c.linear(c.d_model, c.d_ff)
+        s.W3 = c.linear(c.d_model, c.d_ff)
+        s.W2 = c.linear(c.d_ff, c.d_model)
+
+    def __call__(s, x):
+        return s.W2(silu(s.W1(x)) * s.W3(x))
+
+
 class TransformerBlock(nnx.Module):
     def __init__(s, c: LLMConf):
         s.attention = MultiHeadAttention(c)
-        s.W1 = nnx.Linear(c.d_model, c.d_ff, rngs=c.rngs)
-        s.W2 = nnx.Linear(c.d_ff, c.d_model, rngs=c.rngs)
-        s.norm1 = nnx.LayerNorm(c.d_model, rngs=c.rngs)
-        s.norm2 = nnx.LayerNorm(c.d_model, rngs=c.rngs)
-        s.dropout = nnx.Dropout(c.dropout, rngs=c.rngs)
+        s.swiGLU = SwiGLU(c)
+        s.norm1 = RMSNorm(c.d_model)
+        s.norm2 = RMSNorm(c.d_model)
 
     def __call__(s, x: Array):
         # Pre-LayerNorm: more stable gradients than the original Post-LayerNorm
-        x = x + s.dropout(s.attention(s.norm1(x)))
-        x = x + s.dropout(s.W2(gelu(s.W1(s.norm2(x)))))
+        x = x + s.attention(s.norm1(x))
+        x = x + s.swiGLU(s.norm2(x))
         return x
 
 
@@ -107,7 +142,7 @@ class LLM(nnx.Module):
         s.c = c
         s.embed = TextEmbed(c)
         s.blocks = nnx.Sequential(*[TransformerBlock(c) for _ in range(c.n_layer)])
-        s.norm = nnx.LayerNorm(c.d_model, rngs=c.rngs)
+        s.norm = RMSNorm(c.d_model)
 
     def __call__(s, x: Array):
         return s.embed.undo(s.norm(s.blocks(s.embed(x))))
@@ -154,6 +189,7 @@ def main():
     c = LLMConf()
     c.n_vocab = bpe.n_vocab
     c.mask = causal_mask(c.max_T)
+    c.roPE = RoPE(c.max_T, c.d_head)
 
     llm = LLM(c)
     logits = llm(x)
